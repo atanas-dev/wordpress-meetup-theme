704261b6b6403f54562558efe6ce69523a70a36c
Atanas Angelov <hi@atanas.dev>
2019-12-01 16:30:18 +0200

    Add watch and browser-sync for Step 2

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
 
 module.exports = {
   mode: process.env.NODE_ENV,
@@ -7,4 +8,11 @@ module.exports = {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  plugins: [
+    new BrowserSyncPlugin({
+      host: 'localhost',
+      port: 3000,
+      proxy: 'http://wpmeetup.local/'
+    }),
+  ],
 };
